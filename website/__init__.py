cd3504a3e5011ca3554582de31a403e7322fecc0
Filippa <filippa.hognasbacka@gmail.com>
2024-04-17 14:29:57 +0300

    removed SQLAlchemy from init

--- website/__init__.py
+++ website/__init__.py
@@ -1,50 +1,41 @@
 from flask import Flask
-from os import path
 from flask_login import LoginManager
 from dotenv import load_dotenv
 import os
-import sqlite3
+import sys
+from os.path import abspath, dirname
+sys.path.append(dirname(dirname(abspath(__file__))))
+from website.db import get_db
+
 
 load_dotenv()
 
-db = SQLAlchemy()
-DB_NAME = "database.db"
-
-def create_app():
-    app = Flask(__name__)
-
-    app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
-    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
-
-    db.init_app(app)
 
 
-    from .views import views
-    from .auth import auth
+app = Flask(__name__)
 
-    app.register_blueprint(views, url_prefix='/') 
-    app.register_blueprint(auth, url_prefix='/') 
-
-
-    from .models import User, Review
-
-
-
-    with app.app_context():
-        db.create_all()
-
-
-    login_manager = LoginManager()
-    login_manager.login_view = 'auth.login'
-    login_manager.init_app(app)
-
-    @login_manager.user_loader
-    def load_user(id):
-        return User.query.get(int(id))
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
     
-    return app
+from .views import views
+from .auth import auth
 
-def create_database(app):
-    if not path.exists('website/' + DB_NAME):
-        db.create_all(app=app)
-        print('Created Database!')
+app.register_blueprint(views, url_prefix='/') 
+app.register_blueprint(auth, url_prefix='/') 
+
+login_manager = LoginManager()
+login_manager.login_view = 'auth.login'
+login_manager.init_app(app)
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    db = get_db()
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM user WHERE id = ?", (user_id,))
+    user = cursor.fetchone()
+    if user:
+        return user 
+
+
+
+
