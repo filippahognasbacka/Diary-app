8ff9fb2965d48eb3b0045530750e560877b00ce1
Filippa Högnäsbacka <153224468+filippahognasbacka@users.noreply.github.com>
2024-05-03 14:03:31 +0300

    Delete website/static/index.js because not needed anymore

--- website/static/index.js
+++
@@ -1,9 +0,0 @@
-function deleteEntry(entryId) {
-    fetch("/delete-entry", {
-        method: 'POST',
-        body: JSON.stringify({ entryId: entryId }),
-    })
-    .then(function(_res) {
-        window.location.href = "/";
-    });
-}
